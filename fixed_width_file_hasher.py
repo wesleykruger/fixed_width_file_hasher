62f46bf15643003fbc26f2ffac541f1ce3df470b
Wes Kruger <wesleykruger@gmail.com>
2019-09-27 16:55:31 -0500

    completed initial development

--- fixed_width_file_hasher.py
+++ fixed_width_file_hasher.py
@@ -1,51 +1,99 @@
-import os, fnmatch, sys, shutil
 from pathlib import Path
 import random
 import string
 
-input_file = Path('C:/Users/wesleykruger/Documents/BBVA/pii-shuffle/test.txt')
-output_file = Path('C:/Users/wesleykruger/Documents/BBVA/pii-shuffle/test2.txt')
 
+# This must come before the user definition section
 class ScrambleObject:
-    def __init__(self, record_type_start_pos: int, record_type_indicator_length: int, record_value: str, scramble_start_pos: int, scramble_length: int, data_type: str):
+    def __init__(self, record_type_start_pos: int, record_value: str,
+                 scramble_start_pos: int, scramble_length: int, data_type: str):
         self.record_type_start_pos = record_type_start_pos
-        self.record_type_indicator_length = record_type_indicator_length
         self.record_value = record_value
         self.scramble_start_pos = scramble_start_pos
         self.scramble_length = scramble_length
         self.data_type = data_type
 
-#Enter your lines to scramble
+
+# *****************************************************************
+# USER INPUT SECTION
+
+# Define the file you will be reading from and the file you will be writing to.
+# Make sure that the output directory can be written to.
+input_file = Path('')
+output_file = Path('')
+
+# Enter your lines to mask here, each as a new ScrambleObject.
+# Each should follow this format, and they should be separated by commas within the array:
+
+# record_type_start_pos(int): This is where we should begin looking for an indicator that a value needs to be scrambled on this line. If it is the first character, it will be 0.
+# record_value(str): This is the value that we should be looking for to know to scramble a different part of the line.
+# scramble_start_pos(int): This is the first character in the string that needs to be scrambled. Remember it is 0-based.
+# scramble_length(int): This is the length of the string starting at scramble_start_pos that needs to be scrambled.
+# data_type(either 'str' or 'int): This tells us whether the output of the scramble needs to be letters or numbers. The purpose of this is to ensure that the output can still be processed as valid test data.
+# data_type property in each object should be either 'str' or 'int'. Anything else will produce an error.
+
+# Ex: scramble_array = scramble_array = [
+#     ScrambleObject(record_type_start_pos=0, record_value='A', scramble_start_pos=43, scramble_length=18, data_type='str'),
+#     ScrambleObject(record_type_start_pos=0, record_value='C', scramble_start_pos=9, scramble_length=10, data_type='int'),
+# ]
+# All strings are case-sensitive
+
 scramble_array = [
-    ScrambleObject(0, 1, 'c', 2, 4, 'string')
+    ScrambleObject(record_type_start_pos=0, record_value='A', scramble_start_pos=43, scramble_length=18, data_type='str'),
+    ScrambleObject(record_type_start_pos=0, record_value='B', scramble_start_pos=33, scramble_length=9, data_type='str'),
+    ScrambleObject(record_type_start_pos=0, record_value='C', scramble_start_pos=9, scramble_length=10, data_type='int'),
+    ScrambleObject(record_type_start_pos=0, record_value='D', scramble_start_pos=28, scramble_length=1, data_type='int'),
+    ScrambleObject(record_type_start_pos=0, record_value='E', scramble_start_pos=62, scramble_length=5, data_type='int')
 ]
 
+# END USER DEFINITIONS
+# ********************************************************************
 
-def randomString(text_or_int, data_type):
+
+def validate_objects(obj):
+    if type(obj.record_type_start_pos) != int or type(obj.record_value) != str \
+            or type(obj.scramble_start_pos) != int or type(obj.scramble_length) != int:
+        return 'TypeError'
+    elif obj.data_type != 'str' and obj.data_type != 'int':
+        return 'DataType'
+    else:
+        return True
+
+
+def random_string(text_or_int, data_type):
     length = len(text_or_int)
     if data_type == 'int':
-        numbers = list(range(10))
+        numbers = '0123456789'
         characters = ''.join(random.choice(numbers) for i in range(length))
     else:
-        letters = string.ascii_lowercase
+        letters = string.ascii_letters
         characters = ''.join(random.choice(letters) for i in range(length))
     return characters
 
 
-def scramble(scramble_array):
+def scramble(arr):
     lines = []
+    # Want to hold file data in memory to manipulate and then write once at the end rather than multiple writes
     with open(input_file, encoding='utf8') as f:
         s = f.readlines()
         for line in s:
             lines.append(line)
-    for entry in scramble_array:
-        for index, line in enumerate(lines):
-            if line[entry.record_type_start_pos: entry.record_type_start_pos + entry.record_type_indicator_length] == entry.record_value:
+    for index, entry in enumerate(arr):
+        # Zero-based index is confusing for error message
+        error_index = index + 1
+        if validate_objects(entry) == 'TypeError':
+            raise TypeError('You are using the incorrect data type for one of your properties '
+                             'in object number {}'.format(error_index))
+        elif validate_objects(entry) == 'DataType':
+            raise Exception("The data type you selected in scramble object number {} is incorrect. "
+                            "Please select either 'str' or 'int'.".format(error_index))
+        for line_index, line in enumerate(lines):
+            if line[entry.record_type_start_pos: entry.record_type_start_pos + len(entry.record_value)] == entry.record_value:
                 text_to_replace = line[entry.scramble_start_pos:entry.scramble_start_pos + entry.scramble_length]
-                scramble_values = randomString(text_to_replace, entry.data_type)
+                scramble_values = random_string(text_to_replace, entry.data_type)
                 line = line[:entry.scramble_start_pos] + scramble_values + line[entry.scramble_start_pos + len(scramble_values):]
-                print(line)
-            lines[index] = line
+            print(line)
+            lines[line_index] = line
     with open(output_file, 'w', encoding='utf8') as w:
         w.write(''.join(lines))
     w.close()

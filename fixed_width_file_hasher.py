d88b778919788be0e2a8089f2b6b779116e4cbfc
Wes Kruger <wesleykruger@gmail.com>
2019-09-30 10:55:14 -0500

    Changed naming conventions to match client needs

--- fixed_width_file_hasher.py
+++ fixed_width_file_hasher.py
@@ -5,13 +5,13 @@ import os.path
 
 
 # This must come before the user definition section
-class ScrambleObject:
+class RedactObject:
     def __init__(self, record_type_start_pos: int, record_value: str,
-                 scramble_start_pos: int, scramble_length: int, data_type: str):
+                 redact_start_pos: int, redact_length: int, data_type: str):
         self.record_type_start_pos = record_type_start_pos
         self.record_value = record_value
-        self.scramble_start_pos = scramble_start_pos
-        self.scramble_length = scramble_length
+        self.redact_start_pos = redact_start_pos
+        self.redact_length = redact_length
         self.data_type = data_type
 
 
@@ -25,28 +25,28 @@ output_file = Path('')
 
 line_limit = 7
 
-# Enter your lines to mask here, each as a new ScrambleObject.
+# Enter your lines to mask here, each as a new RedactObject.
 # Each should follow this format, and they should be separated by commas within the array:
 
-# record_type_start_pos(int): This is where we should begin looking for an indicator that a value needs to be scrambled on this line. If it is the first character, it will be 0.
-# record_value(str): This is the value that we should be looking for to know to scramble a different part of the line.
-# scramble_start_pos(int): This is the first character in the string that needs to be scrambled. Remember it is 0-based.
-# scramble_length(int): This is the length of the string starting at scramble_start_pos that needs to be scrambled.
-# data_type(either 'str' or 'int): This tells us whether the output of the scramble needs to be letters or numbers. The purpose of this is to ensure that the output can still be processed as valid test data.
+# record_type_start_pos(int): This is where we should begin looking for an indicator that a value needs to be redacted on this line. If it is the first character, it will be 0.
+# record_value(str): This is the value that we should be looking for to know to redact a different part of the line.
+# redact_start_pos(int): This is the first character in the string that needs to be redacted. Remember it is 0-based.
+# redact_length(int): This is the length of the string starting at redact_start_pos that needs to be redacted.
+# data_type(either 'str' or 'int): This tells us whether the output of the redact needs to be letters or numbers. The purpose of this is to ensure that the output can still be processed as valid test data.
 # data_type property in each object should be either 'str' or 'int'. Anything else will produce an error.
 
-# Ex: scramble_array = scramble_array = [
-#     ScrambleObject(record_type_start_pos=0, record_value='A', scramble_start_pos=43, scramble_length=18, data_type='str'),
-#     ScrambleObject(record_type_start_pos=0, record_value='C', scramble_start_pos=9, scramble_length=10, data_type='int'),
+# Ex: redact_array = [
+#     RedactObject(record_type_start_pos=0, record_value='A', redact_start_pos=43, redact_length=18, data_type='str'),
+#     RedactObject(record_type_start_pos=0, record_value='C', redact_start_pos=9, redact_length=10, data_type='int'),
 # ]
 # All strings are case-sensitive
 
-scramble_array = [
-    ScrambleObject(record_type_start_pos=0, record_value='A', scramble_start_pos=43, scramble_length=18, data_type='str'),
-    ScrambleObject(record_type_start_pos=0, record_value='B', scramble_start_pos=33, scramble_length=9, data_type='str'),
-    ScrambleObject(record_type_start_pos=0, record_value='C', scramble_start_pos=9, scramble_length=10, data_type='int'),
-    ScrambleObject(record_type_start_pos=0, record_value='D', scramble_start_pos=28, scramble_length=1, data_type='int'),
-    ScrambleObject(record_type_start_pos=0, record_value='E', scramble_start_pos=62, scramble_length=5, data_type='int')
+redact_array = [
+    RedactObject(record_type_start_pos=0, record_value='A', redact_start_pos=43, redact_length=18, data_type='str'),
+    RedactObject(record_type_start_pos=0, record_value='B', redact_start_pos=33, redact_length=9, data_type='str'),
+    RedactObject(record_type_start_pos=0, record_value='C', redact_start_pos=9, redact_length=10, data_type='int'),
+    RedactObject(record_type_start_pos=0, record_value='D', redact_start_pos=28, redact_length=1, data_type='int'),
+    RedactObject(record_type_start_pos=0, record_value='E', redact_start_pos=62, redact_length=5, data_type='int')
 ]
 
 # END USER DEFINITIONS
@@ -55,7 +55,7 @@ scramble_array = [
 
 def validate_objects(obj):
     if type(obj.record_type_start_pos) != int or type(obj.record_value) != str \
-            or type(obj.scramble_start_pos) != int or type(obj.scramble_length) != int:
+            or type(obj.redact_start_pos) != int or type(obj.redact_length) != int:
         return 'TypeError'
     elif obj.data_type != 'str' and obj.data_type != 'int':
         return 'DataType'
@@ -74,7 +74,7 @@ def random_string(text_or_int, data_type):
     return characters
 
 
-def scramble(arr):
+def redact(arr):
     lines = []
     if not os.path.isfile(input_file):
         raise FileNotFoundError('Input file does not exist, please enter a valid path.')
@@ -87,7 +87,7 @@ def scramble(arr):
             raise TypeError('You are using the incorrect data type for one of your properties '
                             'in object number {}'.format(error_index))
         elif validate_objects(validate_entry) == 'DataType':
-            raise Exception("The data type you selected in scramble object number {} is incorrect. "
+            raise Exception("The data type you selected in redact object number {} is incorrect. "
                             "Please select either 'str' or 'int'.".format(error_index))
 
     # Overwrite output file if it already exists so we're not appending each time
@@ -98,9 +98,9 @@ def scramble(arr):
         for line in fileobject:
             for entry in arr:
                 if line[entry.record_type_start_pos: entry.record_type_start_pos + len(entry.record_value)] == entry.record_value:
-                    text_to_replace = line[entry.scramble_start_pos:entry.scramble_start_pos + entry.scramble_length]
-                    scramble_values = random_string(text_to_replace, entry.data_type)
-                    line = line[:entry.scramble_start_pos] + scramble_values + line[entry.scramble_start_pos + len(scramble_values):]
+                    text_to_replace = line[entry.redact_start_pos:entry.redact_start_pos + entry.redact_length]
+                    redact_values = random_string(text_to_replace, entry.data_type)
+                    line = line[:entry.redact_start_pos] + redact_values + line[entry.redact_start_pos + len(redact_values):]
             lines.append(line)
             if len(lines) >= line_limit:
                 with open(output_file, 'a+', encoding='utf8') as a:
@@ -114,4 +114,4 @@ def scramble(arr):
 
 
 if __name__ == '__main__':
-    scramble(scramble_array)
+    redact(redact_array)
